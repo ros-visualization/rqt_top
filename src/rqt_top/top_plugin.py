75122f8a2d45fb97066d9cd8b30abebeae0647e8
Mike Lautman <mikeblautman@gmail.com>
2018-10-23 10:45:51 -0600

    autopep8 (#5)

--- src/rqt_top/top_plugin.py
+++ src/rqt_top/top_plugin.py
@@ -40,9 +40,10 @@ import textwrap
 
 
 class TopWidgetItem(QTreeWidgetItem):
+
     def __init__(self, parent=None):
         super(TopWidgetItem, self).__init__(parent)
-        
+
     def __lt__(self, other):
         col = self.treeWidget().sortColumn()
         dtype = Top.SORT_TYPE[col]
@@ -50,15 +51,14 @@ class TopWidgetItem(QTreeWidgetItem):
 
 
 class Top(Plugin):
-
     NODE_FIELDS   = [             'pid', 'get_cpu_percent', 'get_memory_percent', 'get_num_threads']
     OUT_FIELDS    = ['node_name', 'pid', 'cpu_percent',     'memory_percent',     'num_threads'    ]
     FORMAT_STRS   = ['%s',        '%s',  '%0.2f',           '%0.2f',              '%s'             ]
     NODE_LABELS   = ['Node',      'PID', 'CPU %',           'Mem %',              'Num Threads'    ]
     SORT_TYPE     = [str,         str,   float,             float,                float            ]
-    TOOLTIPS      = {
+    TOOLTIPS = {
         0: ('cmdline', lambda x: '\n'.join(textwrap.wrap(' '.join(x)))),
-        3: ('memory_info', lambda x: ('Resident: %0.2f MiB, Virtual: %0.2f MiB' % (x[0]/2**20, x[1]/2**20)))
+        3: ('memory_info', lambda x: ('Resident: %0.2f MiB, Virtual: %0.2f MiB' % (x[0] / 2**20, x[1] / 2**20)))
     }
 
     _node_info = NodeInfo()
@@ -75,8 +75,8 @@ class Top(Plugin):
         parser = ArgumentParser()
         # Add argument(s) to the parser.
         parser.add_argument("-q", "--quiet", action="store_true",
-                      dest="quiet",
-                      help="Put plugin in silent mode")
+                            dest="quiet",
+                            help="Put plugin in silent mode")
         args, unknowns = parser.parse_known_args(context.argv())
         # if not args.quiet:
         #     print 'arguments: ', args
@@ -88,7 +88,7 @@ class Top(Plugin):
         # Setup the toolbar
         self._toolbar = QToolBar()
         self._filter_box = QLineEdit()
-        self._regex_box  = QCheckBox()
+        self._regex_box = QCheckBox()
         self._regex_box.setText('regex')
         self._toolbar.addWidget(QLabel('Filter'))
         self._toolbar.addWidget(self._filter_box)
@@ -100,7 +100,7 @@ class Top(Plugin):
         # Create a container widget and give it a layout
         self._container = QWidget()
         self._container.setWindowTitle('Process Monitor')
-        self._layout    = QVBoxLayout()
+        self._layout = QVBoxLayout()
         self._container.setLayout(self._layout)
 
         self._layout.addWidget(self._toolbar)
@@ -174,7 +174,7 @@ class Top(Plugin):
     def shutdown_plugin(self):
         self._update_timer.stop()
 
-    def save_settings(self, plugin_settings, instance_settings):        
+    def save_settings(self, plugin_settings, instance_settings):
         instance_settings.set_value('filter_text', self._filter_box.text())
         instance_settings.set_value('is_regex', int(self._regex_box.checkState()))
 
@@ -187,6 +187,6 @@ class Top(Plugin):
             self._regex_box.setCheckState(Qt.CheckState(0))
         self.update_filter()
 
-    #def trigger_configuration(self):
+    # def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure it
         # Usually used to open a configuration dialog
