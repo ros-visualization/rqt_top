4b9e2cc2c5673c72f2de3ad69458c295fc7da5b8
Stephen <brawner@gmail.com>
2018-11-12 13:24:58 -0700

    Porting rqt_top to ROS2

--- src/rqt_top/node_info.py
+++ src/rqt_top/node_info.py
@@ -25,13 +25,6 @@
 
 # Author Dan Lazewatsky/lazewatd@engr.orst.edu
 
-import rosnode
-import rospy
-try:
-    from xmlrpc.client import ServerProxy
-except ImportError:
-    from xmlrpclib import ServerProxy
-from socket import error as SocketError
 import psutil
 
 ID = '/NODEINFO'
@@ -40,57 +33,78 @@ ID = '/NODEINFO'
 class NodeInfo(object):
     nodes = dict()
 
+    def __init__(self, node):
+        self._node = node
+
     def get_node_info(self, node_name, skip_cache=False):
-        node_api = rosnode.get_api_uri(rospy.get_master(), node_name, skip_cache=skip_cache)
-        try:
-            code, msg, pid = ServerProxy(node_api[2]).getPid(ID)
-            if node_name in self.nodes:
-                return self.nodes[node_name]
-            else:
-                try:
-                    p = psutil.Process(pid)
-                    self.nodes[node_name] = p
-                    return p
-                except:
-                    return False
-        except SocketError:
-            if not skip_cache:
-                return self.get_node_info(node_name, skip_cache=True)
-            else:
+        if node_name not in self.nodes:
+            self.nodes = self.get_all_node_info()
+            if node_name not in self.nodes:
                 return False
+        return self.nodes[node_name]
+
+        # TODO(brawner) ROS2 doesn't currently expose other node's GUIDs or process id in rclpy
+        # node_api = rosnode.get_api_uri(rospy.get_master(), node_name, skip_cache=skip_cache)
+        # try:
+        #     _, _, pid = ServerProxy(node_api[2]).getPid(ID)
+        #     if node_name in self.nodes:
+        #         return self.nodes[node_name]
+        #     else:
+        #         try:
+        #             p = psutil.Process(pid)
+        #             self.nodes[node_name] = p
+        #             return p
+        #         except e:
+        #             return False
+        # except SocketError:
+        #     if not skip_cache:
+        #         return self.get_node_info(node_name, skip_cache=True)
+        #     else:
+        #         return False
 
     def get_all_node_info(self):
         infos = []
-        self.remove_dead_nodes()
-        for node_name in rosnode.get_node_names():
-            info = self.get_node_info(node_name)
-            if info is not False:
-                infos.append((node_name, info))
+        node_names = self._node.get_node_names()
+        for name in node_names:
+            infos.append({'node_name': name})
         return infos
+
+        # TODO(brawner) ROS2 doesn't currently expose other node's GUIDs or process id in rclpy
+        # self.remove_dead_nodes()
+        # for node_name in rosnode.get_node_names():
+        #     info = self.get_node_info(node_name)
+        #     if info is not False:
+        #         infos.append((node_name, info))
+        # return infos
 
     def get_all_node_fields(self, fields):
-        processes = self.get_all_node_info()
-        infos = []
-        psutil_v2_api = int(psutil.__version__.split('.')[0]) >= 2
-        for name, p in processes:
-            all_fields = fields + ['cmdline', 'get_memory_info']
-            if psutil_v2_api:
-                all_fields = [
-                    f[4:] if f.startswith('get_') else f
-                    for f in all_fields]
-            infos.append(self.as_dict(p, all_fields))
-            infos[-1]['node_name'] = name
-        return infos
+        return self.get_all_node_info()
+
+        # TODO(brawner) ROS2 doesn't currently expose other node's GUIDs or process id in rclpy
+        # processes = self.get_all_node_info()
+        # infos = []
+        # psutil_v2_api = int(psutil.__version__.split('.')[0]) >= 2
+        # for name, p in processes:
+        #     all_fields = fields + ['cmdline', 'get_memory_info']
+        #     if psutil_v2_api:
+        #         all_fields = [
+        #             f[4:] if f.startswith('get_') else f
+        #             for f in all_fields]
+        #     infos.append(self.as_dict(p, all_fields))
+        #     infos[-1]['node_name'] = name
+        # return infos
 
     def remove_dead_nodes(self):
-        running_nodes = rosnode.get_node_names()
+        running_nodes = self._node.get_node_names()
         dead_nodes = [node_name for node_name in self.nodes if node_name not in running_nodes]
         for node_name in dead_nodes:
             self.nodes.pop(node_name, None)
 
     def kill_node(self, node_name):
-        success, fail = rosnode.kill_nodes([node_name])
-        return node_name in success
+        # TODO(brawner) Killing nodes not yet implemented in rclpy
+        raise NotImplementedError
+        # success, fail = rosnode.kill_nodes([node_name])
+        # return node_name in success
 
     def as_dict(self, p, attrs=[], ad_value=None):
         # copied code from psutil.__init__ from a newer version
